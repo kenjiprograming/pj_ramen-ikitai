f35ba007d028b370cd03bbba40bfdb140b20995f
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-04-09 21:37:04 +0900

    :white_check_mark: ラーメン情報の一覧を取ってこれる

--- app/Http/Controllers/ApiTestController.php
+++ app/Http/Controllers/ApiTestController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ApiTestController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'ramens';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiTestController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TopController;
 use Illuminate\Foundation\Application;
@@ -38,5 +39,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/ramens', [ApiTestController::class, 'index']);
 
 require __DIR__.'/auth.php';
