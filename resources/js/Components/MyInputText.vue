e2998460dcb2a7155123c3faee69cc1badba9424
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-05-16 20:58:09 +0900

    :lipstick: Admin ramen index

---
+++ resources/js/Components/MyInputText.vue
@@ -0,0 +1,34 @@
+<!-- 途中 -->
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps([
+    'title',
+]);
+
+const inputData = ref('');
+
+</script>
+
+<template>
+    <div class="
+        mb-6
+        ">
+        <div class="
+            mb-2
+            font-medium
+            ">{{ title }}
+        </div>
+        <!-- <div v-if="form.errors.name" class="
+            text-red-500
+            ">{{ form.errors.name }}
+        </div> -->
+        <input type="text" v-model="inputData" @input="$emit('updata:inputData', inputData)" :placeholder="`${title}が入ります`" class="
+            py-2 px-3
+            w-full
+            border-gray-300
+            rounded-lg
+            ">
+    </div>
+</template>
--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -28,7 +28,7 @@ const deleteRamen = (ramen) => {
     </div>
 
     <div v-if="page.props.flash.message"
-        class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+        class="p-4 text-sm text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800"
         role="alert">
         <span class="font-medium">
             {{ page.props.flash.message }}
