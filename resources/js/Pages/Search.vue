4190242807ed779d8a272573bd7a4e0378c7a2bc
kenjiposeidon <i.am.kenji.gido@gmail.com>
2024-06-04 20:47:35 +0900

    :sparkles: Front Ramen search

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -25,6 +25,16 @@ const searchNow = () => {
     router.get(route('ramen.search'), { time: time.value });
     time.value = '';
 }
+
+const open = ref({
+    dist: false,
+    type: false,
+    time: false,
+    taste: false
+});
+const toggle = (condition) => {
+    open.value[condition] = !open.value[condition]
+}
 </script>
 
 <template>
@@ -35,42 +45,42 @@ const searchNow = () => {
         <!-- ここにhtmlを書いていく。bodyタグの中身だけ書けば良い。tailwindcssは勝手に効く。 -->
         <div class="bg-orange-100 m-auto">
             <!-- ヘッダー -->
-            <div class="bg-orange-500 h-8 flex justify-center items-center">
-                <div class="font-semibold text-white bg-red-500 w-11/12">ホームへ戻る</div>
+            <div class="bg-orange-400 h-8 flex justify-center items-center">
+                <div class="font-semibold text-white w-11/12">トップへ戻る</div>
             </div>
         </div>
 
         <!-- ボディー -->
-        <div class="bg-orange-400 mt-4 mb-4">
+        <div class="mt-4 mb-4">
             <div class="w-4/5 mx-auto mb-4 pb-2 text-xl font-semibold border-b border-gray-500">
-                <div class="flex justify-between">
+                <div @click="toggle('dist')" class="flex justify-between hover:cursor-pointer">
                     <div>区から探す</div>
                     <div>↓</div>
                 </div>
-                <div class="flex justify-evenly mt-4 mb-4">
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">中央区</a>
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">浜名区</a>
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">天竜区</a>
+                <div v-show="open.dist" class="flex justify-evenly mt-4 mb-4">
+                    <Link :href="route('ramen.search', { all: '中央区' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">中央区</Link>
+                    <Link :href="route('ramen.search', { all: '浜名区' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">浜名区</Link>
+                    <Link :href="route('ramen.search', { all: '天竜区' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">天竜区</Link>
                 </div>
             </div>
             <div class="w-4/5 mx-auto mb-4 pb-2 text-xl font-semibold border-b border-gray-500">
-                <div class="flex justify-between">
+                <div @click="toggle('type')" class="flex justify-between hover:cursor-pointer">
                     <div>ラーメンの系統から探す</div>
                     <div>↓</div>
                 </div>
-                <div class="flex justify-evenly mt-4 mb-4">
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">濃厚太麺</a>
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">濃厚細麺</a>
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">あっさり太麺</a>
-                    <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">あっさり細麺</a>
+                <div v-show="open.type" class="flex justify-evenly mt-4 mb-4">
+                    <Link :href="route('ramen.search', { type: '濃厚太麺' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">濃厚太麺</Link>
+                    <Link :href="route('ramen.search', { type: '濃厚細麺' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">濃厚細麺</Link>
+                    <Link :href="route('ramen.search', { type: 'あっさり太麺' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">あっさり太麺</Link>
+                    <Link :href="route('ramen.search', { type: 'あっさり細麺' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">あっさり細麺</Link>
                 </div>
             </div>
             <div class="w-4/5 mx-auto mb-4 pb-2 text-xl font-semibold border-b border-gray-500">
-                <div class="flex justify-between">
+                <div @click="toggle('time')" class="flex justify-between hover:cursor-pointer">
                     <div>営業時間から探す</div>
                     <div>↓</div>
                 </div>
-                <div class="mt-4 mb-4">
+                <div v-show="open.time" class="mt-4 mb-4">
                     <div class="flex justify-evenly mb-4">
                         <select v-model="time" class="form-select w-full border-yellow-500 rounded-md border" id="time-select">
                             <option v-for="time in times" :value="time">{{ time }}</option>
@@ -87,43 +97,61 @@ const searchNow = () => {
                 </div>
             </div>
             <div class="w-4/5 mx-auto mb-4 pb-2 text-xl font-semibold border-b border-gray-500">
-                <div class="flex justify-between">
+                <div @click="toggle('taste')" class="flex justify-between hover:cursor-pointer">
                     <div>味から探す</div>
-                    <div>↓</div>
+                    <div class="">↓</div>
                 </div>
-                <div class="mt-4 mb-4">
-                    <div class="flex justify-between mt-4 mb-4">
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">醤油</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">塩</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">味噌</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">豚骨</a>
+                <div v-show="open.taste" class="mt-4 mb-4">
+                    <div class="flex justify-evenly mt-4 mb-4">
+                        <Link :href="route('ramen.search', { taste: '醤油' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">醤油</Link>
+                        <Link :href="route('ramen.search', { taste: '塩' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">塩</Link>
+                        <Link :href="route('ramen.search', { taste: '味噌' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">味噌</Link>
+                        <Link :href="route('ramen.search', { taste: '豚骨' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">豚骨</Link>
                     </div>
-                    <div class="flex justify-between mt-4 mb-4">
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">牛骨</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">煮干し</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">白湯</a>
-                        <a class="px-4 py-2 text-sm border border-gray-400 bg-gray-100">魚介</a>
+                    <div class="flex justify-evenly mt-4 mb-4">
+                        <Link :href="route('ramen.search', { taste: '牛骨' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">牛骨</Link>
+                        <Link :href="route('ramen.search', { taste: '煮干し' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">煮干し</Link>
+                        <Link :href="route('ramen.search', { taste: '白湯' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">白湯</Link>
+                        <Link :href="route('ramen.search', { taste: '魚介' })" class="px-4 py-2 text-sm border border-gray-400 bg-gray-100 hover:bg-orange-500 hover:text-white">魚介</Link>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div v-for="ramen in ramens" class="bg-red-500 w-11/12 mx-auto border-t border-b border-gray-400 py-4 mt-4">
-            <div class="w-11/12 mx-auto">
-                <div class="flex mb-4">
-                    <div class="text-xl font-semibold">{{ ramen.name }}</div>
-                    <div class="rounded border border-gray-400 px-2">{{ ramen.type }}</div>
-                </div>
-                <div class="flex justify-between mb-2">
-                    <div class="mx-2"><img src="/image/noimg.png" alt=""></div>
-                </div>
-                <div class="mb-2">
-                    <div>9:00~20:00（営業時間）</div>
-                </div>
-                <div class="mb-2">
-                    <div>木（休業日）</div>
+        <div v-if="Object.keys(ramens).length > 0">
+            <div v-for="ramen in ramens" class="w-11/12 mx-auto border-t border-b border-gray-400 py-4 mt-4">
+                <div class="w-11/12 mx-auto">
+                    <div class="flex mb-4 justify-center">
+                        <div class="text-xl font-semibold me-4">{{ ramen.name }}</div>
+                        <div class="px-2 text-sm border border-gray-400 bg-gray-100 flex text-center items-center">
+                            <p>{{ ramen.type }}</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-center mb-2">
+                        <div class="mx-2 w-[380px]">
+                            <img v-if="ramen.image" :src="'/upload/'+ ramen.image" :alt="ramen.image" class="object-cover">
+                            <img v-else src="/image/noimg.png" alt="画像なし" class="">
+                        </div>
+                    </div>
+                    <div class="flex justify-center mb-2">
+                        <div>{{ ramen.address }}（住所）</div>
+                    </div>
+                    <div class="flex justify-center mb-2">
+                        <div>{{ ramen.time_open }}~{{ramen.time_close}} <span v-if="ramen.time_open_2">, {{ ramen.time_open_2 }} ~ {{ ramen.time_close_2 }}</span> （営業時間）</div>
+                    </div>
+                    <div class="flex justify-center mb-2">
+                        <div>木（休業日）</div>
+                    </div>
+                    <div class="flex justify-center mb-2">
+                        <div class="bg-orange-500 w-1/12 rounded hover:bg-white">
+                            <Link :href="route('ramen.show', { ramen: ramen.id })" class="block text-white text-center w-full py-2 hover:text-orange-500">詳細</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div v-else>
+            <div class="w-11/12 mx-auto text-center py-4 mt-4">申し訳ありません。検索条件を変えて再度検索してください。</div>
         </div>
     </div>
 
